cc8573671d61bfae32f473cc783128ea645336ef
Mohamed-khaled0 <mohamedalshraby3@gmail.com>
2024-03-19 13:06:47 +0200

    First-Commit

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by MoKhaled on 18/03/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by MoKhaled on 18/03/2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
